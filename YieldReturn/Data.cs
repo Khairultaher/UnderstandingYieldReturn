d6416c152b4c880d7a0b6054869152a466290004
Khairul Taher <khairultaher@outlook.com>
2021-05-19 13:05:56 +0600

    first commit

---
+++ YieldReturn/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldReturn
+{
+    public class Data
+    {
+        List<int> lstNums;
+        public Data()
+        {
+            lstNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        }
+        public IEnumerable<int> RunningTotalWithYield()
+        {
+            int total = 0;
+            foreach (var num in lstNums)
+            {
+                total += num;
+                yield return (total);
+            }
+        }
+        public IEnumerable<int> RunningTotal()
+        {
+            List<int> runningTotal = new List<int>(); int total = 0;
+            foreach (var num in lstNums)
+            {
+                total += num;
+                runningTotal.Add(total);
+            }
+            return runningTotal;
+        }
+    }
+}
---
+++ YieldReturn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YieldReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Data();
+
+            // With Yield 
+            foreach (var item in data.RunningTotalWithYield())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+            // General 
+            foreach (var item in data.RunningTotal())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
